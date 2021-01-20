a223042da90d4b059a854fb2f6c00df2276378b9
Jeana Rose <jr.mathis608@gmail.com>
2021-01-20 15:42:11 -0500

    Added a schema file from MySQL

---
+++ schema.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS employee_DB;
+
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+	salary DECIMAL(10,0),
+    department_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+
+
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES role(id)
+);
+
+
+SELECT * FROM employee;
+
+SELECT * FROM role;
+
+SELECT * FROM department;
