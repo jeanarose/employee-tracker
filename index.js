0588247b31874b4c6a236123dbbe3080c54e3198
Jeana Rose <jr.mathis608@gmail.com>
2021-01-20 15:55:53 -0500

    Wrote basics of init function. Added an exit function

--- index.js
+++ index.js
@@ -3,21 +3,40 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "moose",
-    database: "employee_DB",
-  });
-  
-  connection.connect((err) => {
-    if (err) throw err;
-    console.log(`connected as id: ${connection.threadId}`);
-    // init();
-  });
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "moose",
+  database: "employee_DB",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id: ${connection.threadId}`);
+  init();
+});
+
+const init = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: ["View all employees"],
+      },
+    ])
+    .then(({ action }) => {
+      console.log(action);
+    });
+};
+
+function exit() {
+    connection.end();
+  }
