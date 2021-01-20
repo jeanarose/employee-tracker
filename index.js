84fca7bd9f858f27ba8615ebf341200e60e17516
Jeana Rose <jr.mathis608@gmail.com>
2021-01-20 15:50:43 -0500

    Added connection to index.js. Changed password and database

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 // Dependencies
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "moose",
+    database: "employee_DB",
+  });
+  
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id: ${connection.threadId}`);
+    // init();
+  });
