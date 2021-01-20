e6862a7905cb84a431c1e927de1c018bd3de4939
Jeana Rose <jr.mathis608@gmail.com>
2021-01-20 17:09:40 -0500

    Added seed data

---
+++ db/seeds.sql
@@ -0,0 +1,50 @@
+DROP DATABASE IF EXISTS employee_DB;
+
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+	salary DECIMAL(10,0),
+    department_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+
+
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES role(id)
+);
+
+INSERT INTO department (name)
+VALUES ("Information Systems");
+
+INSERT INTO role (title, salary)
+VALUES ("Software Engineer", 65000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Jeana Rose","Mathis");
+
+
+SELECT * FROM employee;
+
+SELECT * FROM role;
+
+SELECT * FROM department;
+
