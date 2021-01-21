366b3346d70300c277b358a37a9423497fa6475c
Jeana Rose <jr.mathis608@gmail.com>
2021-01-21 09:08:32 -0500

    Updated schema file

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,12 @@ CREATE DATABASE employee_DB;
 
 USE employee_DB;
 
-CREATE TABLE department (
+CREATE TABLE employee (
     id INTEGER NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
     PRIMARY KEY (id)
 );
 
@@ -15,23 +18,29 @@ CREATE TABLE role (
     title VARCHAR(30),
 	salary DECIMAL(10,0),
     department_id INTEGER NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(department_id) REFERENCES department(id)
+    PRIMARY KEY (id)
 );
 
-
-
-CREATE TABLE employee (
+CREATE TABLE department (
     id INTEGER NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INTEGER,
-    manager_id INTEGER,
-    PRIMARY KEY (id),
-    FOREIGN KEY(role_id) REFERENCES role(id),
-    FOREIGN KEY(manager_id) REFERENCES role(id)
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
+-- View all employees 
+SELECT first_name, last_name,title, name AS department, manager_id, salary
+FROM employee
+INNER JOIN role
+    on employee.role_id = role.id 
+INNER JOIN department
+    on role.department_id = department.id;
+
+
+
+-- View employees by department 
+SELECT first_name, last_name, name AS department, title, salary
+FROM employee, department, role
+WHERE employee.role_id = role.id AND role.department_id = 1 AND department.id = 1;
 
 SELECT * FROM employee;
 
@@ -39,6 +48,4 @@ SELECT * FROM role;
 
 SELECT * FROM department;
 
-SELECT first_name, last_name, name AS department, title, salary
-FROM employee, department, role
-WHERE employee.role_id = role.id AND role.department_id = 1 AND department.id = 1
+-- Display managers by name in table
